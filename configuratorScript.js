3c7cd03188e509530b7ae7e94390160d1b1286cb
eniorJR <marquesnooriok@gmail.com>
2024-01-09 17:55:44 +0100

    x

--- configuratorScript.js
+++ configuratorScript.js
@@ -236,7 +236,7 @@ async function printerSerialOptions() {
     async (answer) => {
       if (answer != "") setup.printerOptions.port = answer;
       else setup.printerOptions.port = "/dev/ttyS0";
-      header += "\n🔧 Puerto de la impresora: 🔌    " + answer;
+      header += "\n🔧 Puerto de la impresora: 🔌  " + setup.printerOptions.port;
     }
   );
   clearConsole();
